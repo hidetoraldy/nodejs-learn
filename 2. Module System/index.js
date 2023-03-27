e4721d02a14a35d64bbaeb5eeacba155bf76b5e2
hidetoraldy <hidetoraldy@gmail.com>
2023-03-27 09:03:36 +0700

    initial commit

---
+++ 2. Module System/index.js
@@ -0,0 +1,11 @@
+// const fs = require('fs'); //? core module
+// const cetakNama = require('./coba'); //? local  module
+// const moment = require('moment'); //? third party module/npm module/node modules 
+
+// console.log('hello');
+
+// const cetakNama = require('./coba');
+// const PI = require('./coba');
+const coba = require('./coba');
+
+console.log(coba.cetakNama('Aldy'), coba.PI);
