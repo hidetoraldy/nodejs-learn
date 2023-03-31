9e91ed489e4e38866fbfd783c1407c25f1b70c0f
hidetoraldy <hidetoraldy@gmail.com>
2023-03-31 09:51:58 +0700

    added validator

--- 5. Contact APP/contacts.js
+++ 5. Contact APP/contacts.js
@@ -1,13 +1,15 @@
 const { dir } = require('console');
-const readline = require('readline');
+//const readline = require('readline');
 const fs = require('fs');
+const chalk = require('chalk');
+const validator = require('validator');
 const { resolve } = require('path');
 const { rejects } = require('assert');
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
 
 //? Membuat folder data jika belum ada
 const dirPath = `./data`;
@@ -21,13 +23,13 @@ if (!fs.existsSync(dataPath)) {
     fs.writeFileSync(dataPath, '[]', 'utf-8');
 }
 
-const tulisPertanyaan = (pertanyaan) => {
-    return new Promise((resolve, rejects) => {
-        rl.question(pertanyaan, (nama) => {
-            resolve(nama);
-        })
-    });
-};
+// const tulisPertanyaan = (pertanyaan) => {
+//     return new Promise((resolve, rejects) => {
+//         rl.question(pertanyaan, (nama) => {
+//             resolve(nama);
+//         })
+//     });
+// };
 
 const simpanContact = (nama, email, noHP) => {
     const contact = {
@@ -36,25 +38,36 @@ const simpanContact = (nama, email, noHP) => {
         noHP
     };
 
-    //? cek duplikasi data
-    const duplikat = contacts.find((contact) => contact.email === email);
-        if (duplikat) {
-            console.log('Data contact telah terdaftar, silahkan gunakan data lain!');
-            return false;
-        }
-
     const file = fs.readFileSync('data/contacts.json', 'utf-8');
     const contacts = JSON.parse(file);
+
+    //? cek duplikasi data
+    const duplikat = contacts.find((contact) => contact.email === email || contacts.find((contact) => contact.noHP === noHP));
+        if (duplikat) {
+            console.log(chalk.red.inverse.bold('Data contact telah terdaftar, silahkan gunakan data lain!'));
+            return false;
+        };
+    
+    //? cek validitas email
+    if(!validator.isEmail(email)) {
+            console.log(chalk.red.inverse.bold('Data email tidak valid!'));
+            return false;
+    };
+
+    //? cek validitas noHP
+    if(!validator.isMobilePhone(noHP, 'id-ID')) {
+        console.log(chalk.red.inverse.bold('Data nomor Handphone tidak valid!'));
+    };
+
     contacts.push(contact);
 
     fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
 
-    console.log(`Terima kasih telah memasukkan data.`);
+    console.log(chalk.cyan.inverse.bold(`Terima kasih telah memasukkan data.`));
 
-    rl.close();
+    //rl.close();
 };
 
 module.exports = {
-    tulisPertanyaan,
     simpanContact
 }
