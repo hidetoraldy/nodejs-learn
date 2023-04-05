0a6a86fa0e9f954bb9e74abb685a3c2a4bc6a412
hidetoraldy <hidetoraldy@gmail.com>
2023-04-05 11:57:50 +0700

    finalized

--- 7. Intro ExpressJS/app.js
+++ 7. Intro ExpressJS/app.js
@@ -1,48 +1,84 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const app = express();
 const port = 3000;
 
-const renderHTML = (path, res) => {
-    fs.readFile(path, (e, data) => {
-        if(e) {
-            res.writeHead(404);
-            res.write(`Error: file not found!`);
-        } else {
-            res.write(data);
-        }
-        res.end();
-    });
-};
+app.get('/', (req, res) => {
+    // res.send('Hello World!');
+    res.sendFile('./index.html', {root: __dirname});
+});
 
-http
-    .createServer((req, res) => {
-        // console.log(url);
-        res.writeHead(200, {
-            'Content-Type' : 'text/html',
-        });
-        
-        const url = req.url;
+app.get('/about', (req, res) => {
+    // res.send('Halaman About');
+    // res.json({
+    //     nama : 'Aldy',
+    //     umur: 24,
+    // });
+    res.sendFile('./about.html', {root: __dirname});
+});
 
-        switch(url){
-            case '/about':
-                renderHTML('./about.html', res);
-                break;
-            case '/contact':
-                renderHTML('./contact.html', res);
-                break;
-            default:
-                renderHTML('./index.html', res);
-                break;
-        }
+app.get('/contact', (req, res) => {
+    // res.send('Halaman contact');
+    res.sendFile('./contact.html', {root: __dirname});
+});
 
-        // if(url === '/about'){
-        //     renderHTML('./about.html', res);
-        // } else if (url === '/contact'){
-        //     renderHTML('./contact.html', res);
-        // } else {
-        //     renderHTML('./index.html', res)
-        // }
-    })
-    .listen(port, () => {
-        console.log(`Server is listening on port ${port}..`);
-    });
+app.get('/product/:id', (req, res) => {
+    res.send(`Product ID : ${req.params.id} <br> Category : ${req.query.category}`);
+});
+
+app.use('/', (req, res) => {
+    res.status(404);
+    res.send('<h1>404</h1>');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+
+// const http = require('http');
+// const fs = require('fs');
+// const port = 3000;
+
+// const renderHTML = (path, res) => {
+//     fs.readFile(path, (e, data) => {
+//         if(e) {
+//             res.writeHead(404);
+//             res.write(`Error: file not found!`);
+//         } else {
+//             res.write(data);
+//         }
+//         res.end();
+//     });
+// };
+
+// http
+//     .createServer((req, res) => {
+//         // console.log(url);
+//         res.writeHead(200, {
+//             'Content-Type' : 'text/html',
+//         });
+
+//         const url = req.url;
+
+//         switch(url){
+//             case '/about':
+//                 renderHTML('./about.html', res);
+//                 break;
+//             case '/contact':
+//                 renderHTML('./contact.html', res);
+//                 break;
+//             default:
+//                 renderHTML('./index.html', res);
+//                 break;
+//         }
+
+//         // if(url === '/about'){
+//         //     renderHTML('./about.html', res);
+//         // } else if (url === '/contact'){
+//         //     renderHTML('./contact.html', res);
+//         // } else {
+//         //     renderHTML('./index.html', res)
+//         // }
+//     })
+//     .listen(port, () => {
+//         console.log(`Server is listening on port ${port}..`);
+//     });
